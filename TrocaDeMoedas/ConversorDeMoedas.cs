452e60f4c928069ea32f7232b373da9232d87fc6
ArturChiodiSiqueira <arturchiodi@gmail.com>
2022-04-19 15:56:37 -0300

    Add project files.

---
+++ TrocaDeMoedas/ConversorDeMoedas.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrocaDeMoedas
+{
+    internal class ConversorDeMoedas
+    {
+        public void DolaraAmericano()
+        {
+            double taxaIOF = 6.38, cotacaoDolar, QuantiaDolar, totalPago;
+
+            Console.WriteLine("\nPARA QUE VOCÊ NÃO TENHA DIFICULDADES NA SUA COMPRA DE DÓLARES,\nPREENCHA O CAMPO ABAIXO CORRETAMENTE!!\n\n");
+
+            Console.Write("qual a cotação do dólar no exato momento da compra? ");
+            cotacaoDolar = double.Parse(Console.ReadLine());
+
+            Console.Write("quantos dólares você pretende comprar? ");
+            QuantiaDolar = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("sendo a taxa IOF = 6,38%");
+
+            totalPago = QuantiaDolar * cotacaoDolar;
+            totalPago = totalPago + totalPago * taxaIOF / 100.0;
+            Console.WriteLine("\nO VALOR A SER PAGO EM REAIS: R$ " + totalPago.ToString("F2"));
+
+            Console.WriteLine("\n\n pressione qualquer tecla para retornar ao menu.");
+            Console.ReadKey();
+        }
+
+        public void Euro()
+        {
+            double taxaIOF = 6.38, cotacaoEuro, QuantiaEuro, totalPago;
+
+            Console.WriteLine("\nPARA QUE VOCÊ NÃO TENHA DIFICULDADES NA SUA COMPRA DE EUROS,\nPREENCHA O CAMPO ABAIXO CORRETAMENTE!!\n\n");
+
+            Console.Write("qual a cotação do euro no exato momento da compra? ");
+            cotacaoEuro = double.Parse(Console.ReadLine());
+
+            Console.Write("quantos euros você pretende comprar? ");
+            QuantiaEuro = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("sendo a taxa IOF = 6,38%");
+
+            totalPago = QuantiaEuro * cotacaoEuro;
+            totalPago = totalPago + totalPago * taxaIOF / 100.0;
+            Console.WriteLine("\nO VALOR A SER PAGO EM REAIS: R$ " + totalPago.ToString("F2"));
+
+            Console.WriteLine("\n\n pressione qualquer tecla para retornar ao menu.");
+            Console.ReadKey();
+        }
+
+        public void LibraEsterlina()
+        {
+            double taxaIOF = 6.38, cotacaoLibra, QuantiaLibra, totalPago;
+
+            Console.WriteLine("\nPARA QUE VOCÊ NÃO TENHA DIFICULDADES NA SUA COMPRA DE LIBRAS,\nPREENCHA O CAMPO ABAIXO CORRETAMENTE!!\n\n");
+
+            Console.Write("qual a cotação da libra no exato momento da compra? ");
+            cotacaoLibra = double.Parse(Console.ReadLine());
+
+            Console.Write("quantas libras você pretende comprar? ");
+            QuantiaLibra = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("sendo a taxa IOF = 6,38%");
+
+            totalPago = QuantiaLibra * cotacaoLibra;
+            totalPago = totalPago + totalPago * taxaIOF / 100.0;
+            Console.WriteLine("\nO VALOR A SER PAGO EM REAIS: R$ " + totalPago.ToString("F2"));
+
+            Console.WriteLine("\n\n pressione qualquer tecla para retornar ao menu.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TrocaDeMoedas/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace TrocaDeMoedas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Menu();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static int Menu()
+        {
+            int opcao;
+            ConversorDeMoedas selecao = new ConversorDeMoedas();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("PARA VOCÊ QUE DESEJA COMPRAR MOEDAS ESTRANGEIRAS, ESTE PROGRAMA IRÁ TE AJUDAR!!!\n");
+            Console.WriteLine("DE ACORDO COM AS OPÇÕES DO MENU, QUAL COMPRA DESEJA EXECUTAR?");
+
+            do
+            {
+                Console.WriteLine("\t _______________________________");
+                Console.WriteLine("\t| $ € £ $ € £  MENU $ € £ $ € £ |");
+                Console.WriteLine("\t|                               |");
+                Console.WriteLine("\t|  opção 0 : sair               |");
+                Console.WriteLine("\t|                               |");
+                Console.WriteLine("\t|  opção 1 : DÓLARES em REAIS   |");
+                Console.WriteLine("\t|  opção 2 : EUROS em REAIS     |");
+                Console.WriteLine("\t|  opção 3 : LIBRAS em REAIS    |");
+                Console.WriteLine("\t|_______________________________|");
+
+                Console.Write("\n\tinforme a opção desejada no MENU: ");
+                opcao = int.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.White;
+
+                if (opcao < 0 || opcao > 3)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n\tOPÇÃO INVALIDA! Para voltar ao menu, pressione QUALQUER TECLA!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ReadKey();
+                    Console.Clear();
+                    Menu();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    switch (opcao)
+                    {
+                        case 0:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nvocê escolheu sair.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            opcao = 0;
+                            break;
+                            
+                        case 1:
+                            selecao.DolaraAmericano();
+                            Console.Clear();
+                            Menu();
+                            break;
+
+                        case 2:
+                            selecao.Euro();
+                            Console.Clear();
+                            Menu();
+                            break;
+
+                        case 3:
+                            selecao.LibraEsterlina();
+                            Console.Clear();
+                            Menu();
+                            break;
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                return opcao;
+            } while (opcao != 0);
+        }
+    }
+}
